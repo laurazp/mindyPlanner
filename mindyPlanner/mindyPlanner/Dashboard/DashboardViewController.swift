bb4a6a13dd5049ea096645792a19fa22ec0d9038
laurazp <laurikazp@gmail.com>
2023-03-24 16:46:07 +0000

    TableView added in Dashboard and configured to work with test array

--- mindyPlanner/mindyPlanner/Dashboard/DashboardViewController.swift
+++ mindyPlanner/mindyPlanner/Dashboard/DashboardViewController.swift
@@ -2,13 +2,26 @@
 import UIKit
 
 class DashboardViewController: UIViewController {
-
+    
+    @IBOutlet weak var remindersTableView: UITableView!
     @IBOutlet weak var addReminderButton: UIButton!
+    var remindersList: [String] = ["Test1", "Test2", "Test3"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "Dashboard"
+        setupTable()
         setupButton()
+    }
+    
+    private func setupTable() {
+        remindersTableView.delegate = self
+        remindersTableView.dataSource = self
+        remindersTableView.rowHeight = UITableView.automaticDimension
+        definesPresentationContext = true
+        //TODO: Delete when CardViews are implemented??
+        remindersTableView.backgroundColor = UIColor.white.withAlphaComponent(0.0)
+        remindersTableView.reloadData()
     }
     
     private func setupButton() {
@@ -21,5 +34,29 @@ class DashboardViewController: UIViewController {
 
     @objc func addReminderButtonPressed() {
         print("Add Reminder Button pressed")
+    }
+    
+    private func configureCell(cell: RemindersCell, indexPath: IndexPath) {
+        cell.remindersTitleLabel.text = remindersList[indexPath.row]
+        print(remindersList[indexPath.row])
+    }
+}
+
+extension DashboardViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return remindersList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "RemindersCell", for: indexPath) as? RemindersCell else { return UITableViewCell() }
+        cell.backgroundColor = UIColor.white.withAlphaComponent(0.0)
+        configureCell(cell: cell, indexPath: indexPath)
+        cell.indexPath = indexPath
+        //cell.delegate = self
+        return cell
     }
 }
