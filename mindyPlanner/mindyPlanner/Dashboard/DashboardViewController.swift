e37e6ca3d8f8b6554c2bad389eaa47648ac0a072
laurazp <laurikazp@gmail.com>
2023-03-24 12:57:59 +0000

    AddRemindersButton added in Dashboard

--- mindyPlanner/mindyPlanner/Dashboard/DashboardViewController.swift
+++ mindyPlanner/mindyPlanner/Dashboard/DashboardViewController.swift
@@ -3,9 +3,23 @@ import UIKit
 
 class DashboardViewController: UIViewController {
 
+    @IBOutlet weak var addReminderButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "Dashboard"
-        
+        setupButton()
+    }
+    
+    private func setupButton() {
+        addReminderButton.layer.cornerRadius = addReminderButton.frame.width / 2
+        addReminderButton.clipsToBounds = true
+        addReminderButton.setImage(UIImage(named:"thumbsUp.png"), for: .normal)
+        addReminderButton.backgroundColor = .white
+        addReminderButton.addTarget(self, action: #selector(addReminderButtonPressed), for: .touchUpInside)
+    }
+
+    @objc func addReminderButtonPressed() {
+        print("Add Reminder Button pressed")
     }
 }
